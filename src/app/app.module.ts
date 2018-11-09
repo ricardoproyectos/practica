75a8d584bc2f3f94a2d8074a73891a31a5bf7851
Espinosa <Ricardo.Espinosa.consultant@nielsen.com>
2018-11-08 18:58:46 -0600

    create form and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +13,8 @@ import { FormComponent } from './components/form/form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-form',
@@ -8,14 +11,24 @@ import { Component } from '@angular/core';
 export class FormComponent  {
   listForm: any = [];
   model: any = {};
+  constructor(private http: HttpClient) {}
 
   onSubmit() {
-    let data = JSON.stringify(this.model);
-    console.log(data)
+    let formdata = JSON.stringify(this.model);
+    // console.log(data)
     // this.listForm = this.model.push();
     // console.log(this.listForm);
+    this.http.get(formdata).subscribe(data => {
+      console.log(data);
+    });
 
   }
+
+  ngOnInit(): void {
+    // Make the HTTP request:
+    
+  }
+
 }
 
 
